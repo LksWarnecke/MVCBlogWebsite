32be5d1910a83da556c2c93f818d9b0e864fbbc9
LksWarnecke <lukaslaptop24@gmail.com>
2025-03-15 00:16:05 +0100

    authdb migration and creating db

--- MVCBlogWebsite/Data/AuthDbContext.cs
+++ MVCBlogWebsite/Data/AuthDbContext.cs
@@ -1,31 +1,41 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace MVCBlogWebsite.Data
 {
     public class AuthDbContext : IdentityDbContext
     {
-        public AuthDbContext(DbContextOptions options) : base(options)
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
         {
-            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            // Suppress PendingModelChangesWarning
+            optionsBuilder.ConfigureWarnings(warnings =>
+                warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+
             // Seed Roles (User, Admin, SuperAdmin)
 
-            var adminRoleId = "94ff21e2-5628-4775-b68a-89396d9ffb85";
-            var superAdminRoleId = "5bc1ad8a-2ba7-4751-b6ab-dd6eb1c80dc7";
-            var userRoleId = "dbb28f37-4948-4562-8302-4a4e27da4b77";
+            var adminRoleId = "37cc67e1-41ca-461c-bf34-2b5e62dbae32";
+            var superAdminRoleId = "3cfd9eee-08cb-4da3-9e6f-c3166b50d3b0";
+            var userRoleId = "a0cab2c3-6558-4a1c-be81-dfb39180da3d";
 
             var roles = new List<IdentityRole>
             {
                 new IdentityRole
                 {
-                    Name = "Admin",
+                    Name= "Admin",
                     NormalizedName = "Admin",
                     Id = adminRoleId,
                     ConcurrencyStamp = adminRoleId
@@ -49,41 +59,45 @@ namespace MVCBlogWebsite.Data
             builder.Entity<IdentityRole>().HasData(roles);
 
             // Seed SuperAdminUser
-            var superAdminId = "f1560bcc-1be0-4178-959f-51f77764978c";
+            var superAdminId = "472ba632-6133-44a1-b158-6c10bd7d850d";
             var superAdminUser = new IdentityUser
             {
-                UserName = "superadmin@blog.com",
-                Email = "superadmin@blog.com".ToUpper(),
-                NormalizedEmail = "superadmin@blog.com".ToUpper(),
-                NormalizedUserName = "superadmin@blog.com".ToUpper(),
+                UserName = "superadmin@bloggie.com",
+                Email = "superadmin@bloggie.com",
+                NormalizedEmail = "superadmin@bloggie.com".ToUpper(),
+                NormalizedUserName = "superadmin@bloggie.com".ToUpper(),
                 Id = superAdminId
             };
 
-            superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(superAdminUser, "Superadmin@123");
+            superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>()
+                .HashPassword(superAdminUser, "Superadmin@123");
+
 
             builder.Entity<IdentityUser>().HasData(superAdminUser);
 
-            // Add all roles to SuperAdminUser
+
+            // Add All roles to SuperAdminUser
             var superAdminRoles = new List<IdentityUserRole<string>>
             {
                 new IdentityUserRole<string>
                 {
                     RoleId = adminRoleId,
-                    UserId = superAdminUser.Id
+                    UserId = superAdminId
                 },
                 new IdentityUserRole<string>
                 {
                     RoleId = superAdminRoleId,
-                    UserId = superAdminUser.Id
+                    UserId = superAdminId
                 },
                 new IdentityUserRole<string>
                 {
                     RoleId = userRoleId,
-                    UserId = superAdminUser.Id
+                    UserId = superAdminId
                 }
             };
 
             builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
+
         }
     }
 }
